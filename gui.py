67bfe7bf186a6c3e5a5779ec2a4209802f24519a
Furglitch <samsonger@proton.me>
2025-09-07 22:45:36 -0500

    Add url checking

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import re
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 from PyQt6.QtWidgets import *
@@ -14,11 +15,11 @@ class stepURL(QDialog):
 		layout.addWidget(self.label)
 
 		self.url_input = QLineEdit()
-		self.url_input.setPlaceholderText("https://next.nexusmods.com/.../collections/.../")
+		self.url_input.setPlaceholderText("https://nexusmods.com/games/.../collections/.../")
 		layout.addWidget(self.url_input)
 
 		self.submit_btn = QPushButton("Submit")
-		self.submit_btn.clicked.connect(self.get_url)
+		self.submit_btn.clicked.connect(self.submit)
 		layout.addWidget(self.submit_btn) 
 
 		self.setLayout(layout)
@@ -29,6 +30,21 @@ class stepURL(QDialog):
 		for suffix in ["/mods", "/comments", "/changelog", "/bugs"]:
 			if input.endswith(suffix):
 				input = input[: -len(suffix)]
-
 		qDebug(f"[NXMColDL] URL entered: {input}")
-		return input
+		return self.check_valid(input)
+
+	def check_valid(self, url):
+		regex = r"^https:\/\/www\.nexusmods\.com\/games\/([a-zA-Z0-9_\-]+)\/collections\/([a-zA-Z0-9_\-]+)\/?$"
+		valid = re.match(regex, url)
+		if valid:
+			qDebug("[NXMColDL] URL is valid")
+			return valid
+
+	def submit(self):
+		matched = self.get_url()
+		if not matched: QMessageBox.critical(self, "Error", "The URL you entered is not a valid Nexus Collection URL."); return
+		self.game = matched.group(1)
+		qDebug(f"[NXMColDL] Game: {self.game}")
+		self.collection = matched.group(2)
+		qDebug(f"[NXMColDL] Collection ID: {self.collection}")
+		self.close()
